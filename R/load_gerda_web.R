6ea4a73b4086087a63655e6d4c7573a385361036
Hanno <hannohilbig@gmail.com>
2024-10-19 13:04:33 -1000

    rds/csv argument added

--- R/load_gerda_web.R
+++ R/load_gerda_web.R
@@ -4,12 +4,13 @@
 #'
 #' @param file_name A character string specifying the name of the file to load. For a list of available data, see \code{\link{gerda_data_list}}.
 #' @param verbose A logical value indicating whether to print additional messages to the console. Default is FALSE.
+#' @param file_format A character string specifying the format of the file. Must be either "csv" or "rds". Default is "rds".
 #'
 #' @return A tibble containing the loaded data.
 #'
 #' @examples
 #' \dontrun{
-#' data_municipal_harm <- load_gerda_web("municipal_harm", verbose = TRUE)
+#' data_municipal_harm <- load_gerda_web("municipal_harm", verbose = TRUE, file_format = "rds")
 #' }
 #'
 #' @import dplyr
@@ -17,45 +18,62 @@
 #' @import readr
 #' @export
 
-load_gerda_web <- function(file_name, verbose = FALSE) {
+load_gerda_web <- function(file_name, verbose = FALSE, file_format = "rds") {
     # Load data dict
-    data_dictionary <- structure(list(data_name = c(
-        "municipal_unharm", "municipal_harm",
-        "state_unharm", "state_harm", "federal_muni_raw", "federal_muni_unharm",
-        "federal_muni_harm", "federal_cty_unharm", "federal_cty_harm",
-        "ags_crosswalks", "cty_crosswalks", "ags_area_pop_emp", "cty_area_pop_emp"
-    ), description = c(
-        "Local elections at the municipal level (1990–2020, unharmonized).",
-        "Local elections at the municipal level (1990–2020, harmonized).",
-        "State elections at the municipal level (2006–2019, unharmonized).",
-        "State elections at the municipal level (2006–2019, harmonized).",
-        "Federal elections at the municipal level (1980–2021, raw data).",
-        "Federal elections at the municipal level (1980–2021, unharmonized).",
-        "Federal elections at the municipal level (1990–2021, harmonized).",
-        "Federal elections at the county level (1953–2021, unharmonized).",
-        "Federal elections at the county level (1990–2021, harmonized).",
-        "Crosswalks for municipalities (1990–2021).", "Crosswalks for counties (1990–2021).",
-        "Crosswalk covariates (area, population, employment) for municipalities (1990–2021).",
-        "Crosswalk covariates (area, population, employment) for counties (1990–2021)."
-    ), csv_url = c(
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_unharm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_harm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_unharm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_harm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_raw.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_unharm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_harm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_unharm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_harm.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/ags_crosswalks.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/cty_crosswalks.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_municipality/final/ags_area_pop_emp.csv?download=",
-        "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_county/final/cty_area_pop_emp.csv?download="
-    )), class = "data.frame", row.names = c(NA, -13L))
+    data_dictionary <- data.frame(
+        data_name = c(
+            "municipal_unharm", "municipal_harm", "state_unharm", "state_harm",
+            "federal_muni_raw", "federal_muni_unharm", "federal_muni_harm",
+            "federal_cty_unharm", "federal_cty_harm", "ags_crosswalks",
+            "cty_crosswalks", "ags_area_pop_emp", "cty_area_pop_emp"
+        ),
+        description = c(
+            "Local elections at the municipal level (1990–2020, unharmonized).",
+            "Local elections at the municipal level (1990–2020, harmonized).",
+            "State elections at the municipal level (2006–2019, unharmonized).",
+            "State elections at the municipal level (2006–2019, harmonized).",
+            "Federal elections at the municipal level (1980–2021, raw data).",
+            "Federal elections at the municipal level (1980–2021, unharmonized).",
+            "Federal elections at the municipal level (1990–2021, harmonized).",
+            "Federal elections at the county level (1953–2021, unharmonized).",
+            "Federal elections at the county level (1990–2021, harmonized).",
+            "Crosswalks for municipalities (1990–2021).",
+            "Crosswalks for counties (1990–2021).",
+            "Crosswalk covariates (area, population, employment) for municipalities (1990–2021).",
+            "Crosswalk covariates (area, population, employment) for counties (1990–2021)."
+        ),
+        csv_url = c(
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_unharm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_harm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_unharm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_harm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_raw.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_unharm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_harm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_unharm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_harm.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/ags_crosswalks.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/cty_crosswalks.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_municipality/final/ags_area_pop_emp.csv?download=",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_county/final/cty_area_pop_emp.csv?download="
+        ),
+        rds_url = c(
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_unharm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/municipal_elections/final/municipal_harm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_unharm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/state_elections/final/state_harm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_raw.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_unharm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/municipality_level/final/federal_muni_harm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_unharm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/federal_elections/county_level/final/federal_cty_harm.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/ags_crosswalks.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/crosswalks/final/cty_crosswalks.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_municipality/final/ags_area_pop_emp.rds",
+            "https://github.com/awiedem/german_election_data/raw/refs/heads/main/data/covars_county/final/cty_area_pop_emp.rds"
+        )
+    )
 
-    # if (!exists("data_dictionary")) {
-    #     stop("Data dictionary not found. Please ensure the package is installed correctly.")
-    # }
 
     # Check if file_name is in data_dict
 
@@ -89,21 +107,28 @@ load_gerda_web <- function(file_name, verbose = FALSE) {
         message("File name found in data dictionary")
     }
 
-    # Get the url
+    # Check if file_format is valid
+    if (!file_format %in% c("csv", "rds")) {
+        stop("Invalid file_format. Must be either 'csv' or 'rds'.")
+    }
 
+    # Get the url
     url <- data_dictionary %>%
         filter(data_name == file_name) %>%
-        pull(csv_url)
+        pull(!!sym(paste0(file_format, "_url")))
 
     if (verbose) {
         message("URL found: ", url)
         message("Loading data...")
     }
 
-    # Load the data
+    # Load the data based on the file format
     data <- tryCatch(
         {
-            read_csv(url)
+            switch(file_format,
+                "csv" = read_csv(url),
+                "rds" = read_rds(url)
+            )
         },
         error = function(e) {
             stop("Error loading data: ", e$message)
